ff77406147c89be9a3b362bd4430513131f220fb
Dominik Kroskowski <kros.domin@gmail.com>
2022-01-23 12:41:48 +0100

    Add class item,rocket, interface spaceship

---
+++ src/com/company/Item.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Item {
+    String name;
+    int weight;
+
+    public Item(String name, int weight){
+        this.name = name;
+        this.weight = weight;
+    }
+}
---
+++ src/com/company/Rocket.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Rocket implements SpaceShip{
+
+    int cost;
+    int weight;
+    int maxWeight;
+    double launchExplosion;
+    double landingCrash;
+    int currentWeight;
+
+    @Override
+    public boolean launch() {
+        return true;
+    }
+
+    @Override
+    public boolean land() {
+        return true;
+    }
+
+    @Override
+    public boolean canCarry(Item item) {
+        return this.currentWeight + item.weight < maxWeight;
+    }
+
+    @Override
+    public double carry(Item item) {
+        return this.currentWeight += item.weight;
+    }
+}
